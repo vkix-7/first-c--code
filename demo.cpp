87f295db05787aa55f0d07779782247b88ffd765
vkix7 <vijayku123456789@gmail.com>
2023-12-01 23:48:45 +0530

    created max function

--- demo.cpp
+++ demo.cpp
@@ -2,8 +2,7 @@
 using namespace std;
 
 int main() {    
-   string s = "vikix_7" ;
-   for(int i = 1 ; i < 6; i++){
-    cout << s << "\n";
-   }
-   }
+   string s = "vikix_7";
+   cout << "hey buddy " << s[6] << s[3] << s[0] ;
+    return 0;
+}
---
+++ max-function.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+int max(int x,int y) { 
+    if(x>y){ 
+        cout << x;
+} 
+    cout << y;
+}
+int main() { 
+    int x , y ;
+    cin >> x >> y ;
+ max(x, y);
+   }
