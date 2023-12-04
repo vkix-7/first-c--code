0b2f856e5dddc069a22d4bdf0d3d50d070794242
vkix7 <vijayku123456789@gmail.com>
2023-12-04 13:57:36 +0530

    added

--- pattern.cpp
+++ pattern.cpp
@@ -44,12 +44,19 @@ void nStarDiamond(int n) {
           cout << endl;
     }
 }
+/*n=5;
+1        1
+12      21
+123    321
+1234  4321
+1234554321
+*/
 void pattern2(int n){
   int space = 2*(n-1);
-for(int i =1; i<n;i++){
+for(int i =1 ; i<=n;i++){
   //numbers
   for(int j=1;j<=i;j++){
-    cout << j;
+    cout << j ;
   }
   //space
   for(int j=1;j<=space;j++){
@@ -57,16 +64,81 @@ for(int i =1; i<n;i++){
   }
   //numbers
   for(int j=i;j>=1;j--){
-    cout << j;
+    cout << j ;
   }
   cout<< endl;
   space-=2;
 
 }
 }
+/* n=5
+1
+0 1 
+1 0 1
+0 1 0 1
+1 0 1 0 1*/
+void pattern3(int n){
+  for(int i=0;i<n;i++){
+int start=1;
+    if(i%2==0) start=1;
+    else start=0;
+    for(int j=0;j<=i;j++){
+      cout << start<<" ";
+      start=1-start;
+    }
+    cout<<endl;
+  }
+}
+void pattern4(int n){int num=1;
+  for(int i=1;i<=n;i++){
+    for(int j=1;j<=i;j++){
+      cout << num <<" ";
+      num+=1;
+    }cout << endl;
+  }
+}
+void pattern5(int n){
+    // Write your code here.
+    for(int i=0;i<n;i++){
+        for(char j='A';j<='A'+(n-i-1);j++){
+            cout << j<<" ";
+        }
+        cout<< endl;
+    }
+}
+void  pattern6(int n)
+   {
+    // Write your code here.
+    for(int i=0;i<n;i++){
+        char ch = 'A';
+        for(int j=0;j<=i;j++){
+            cout << ch << " ";
+            ch++;
+        }
+        cout << endl;
+       
+    }
+}
+void pattern7(int n){
+  for(int i=0;i<n;i++){
+        char ch = 'A'+i;
+        for(int j=0;j<=i;j++){
+            cout << ch << " ";
+            
+        }
+        cout << endl;
+      
+    }
+}
 int main(){
     int n;
     cin >> n ;
-    nStarDiamond(n);
-    pattern2(n);
+    nStarDiamond(n);cout<< endl;
+    pattern2(n);cout<< endl;
+    pattern3(n);cout<< endl;
+pattern4(n);cout<< endl;
+pattern5(n);
+cout<< endl;
+pattern6(n);cout<< endl;
+pattern7(n);cout<< endl;
 }
