82a2092262e6379f552c14fdc686089a483f3d0c
vkix7 <vijayku123456789@gmail.com>
2023-12-05 22:44:49 +0530

    fix error

--- pattern.cpp
+++ pattern.cpp
@@ -150,14 +150,52 @@ void pattern8(int n){
     }
 }
 void pattern9(int n){
-      for(int i=0;i<n;i++){
-        for(char ch = 'E'-i;ch<='E';ch++){
+   for(int i=0;i<n;i++){
+       char ch ='A'+n-1;
+        for(int j=1;j<=i+1;j++){
+            
 cout << ch << " ";
+ch--;
         }
         cout << endl;
     }
 }
+void pattern10(int n){
+for(int i=0;i<n;i++){
+  //stars
+  for(int j=1;j<=n-i;j++){
+    cout << "*";
+  }
+  //spaces
+  for(int j=1;j<=2*i;j++){
+    cout << " ";
+  }
 
+  //stars
+    for(int j=1;j<=n-i;j++){
+    cout << "*";
+  }
+  cout << endl;
+}
+int ch =2*n-2;
+for(int i=1;i<=n;i++){
+  //stars
+  for(int j=1;j<=i;j++){
+    cout << "*";
+  }
+  //spaces
+  for(int j=1;j<=ch;j++){
+    cout << " ";
+  }
+
+  //stars
+    for(int j=1;j<=i;j++){
+    cout << "*";
+  }
+  cout << endl;
+  ch-=2;
+}
+}
 
 int main(){
     int n;
@@ -171,5 +209,6 @@ cout<< endl;
 pattern6(n);cout<< endl;
 pattern7(n);cout<< endl;
 pattern8(n); cout<<endl;
-pattern9(n);
+pattern9(n);cout <<"\n" ;
+pattern10(n);
 }
